d27673a8090300565fc864b2f87d7b33ede8b6f3
凤云鹏 <FYP@MAC-MFQdeMacBook-Air.local>
2017-05-14 20:43:53 +0800

    第一次提交

---
+++ FYPDBManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FYPDBManager
+//
+//  Created by 凤云鹏 on 2017/5/14.
+//  Copyright © 2017年 FYP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FYPDBManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FYPDBManager
+//
+//  Created by 凤云鹏 on 2017/5/14.
+//  Copyright © 2017年 FYP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
